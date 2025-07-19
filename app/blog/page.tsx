69087999064999a5983473e5e88f71a3ec2b2edc
TamimOp <tamimshadanik@gmail.com>
2025-07-19 19:38:36 +0600

    Add BlogSectionOne component for blog page structure

---
+++ app/blog/page.tsx
@@ -0,0 +1,12 @@
+import BlogSectionOne from "@/components/blog/BlogSectionOne";
+import React from "react";
+
+const page = () => {
+  return (
+    <main>
+      <BlogSectionOne />
+    </main>
+  );
+};
+
+export default page;
---
+++ components/blog/BlogSectionOne.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BlogSectionOne = () => {
+  return <div>Hello</div>;
+};
+
+export default BlogSectionOne;
