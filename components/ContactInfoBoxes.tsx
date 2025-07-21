731cadb099988f3ca85748ef7bd1f7d511abca58
TamimOp <tamimshadanik@gmail.com>
2025-07-21 16:37:43 +0600

    Update ContactInfoBoxes component with new SVG icons and enhanced styling

--- components/ContactInfoBoxes.tsx
+++ components/ContactInfoBoxes.tsx
@@ -2,17 +2,17 @@ import Image from "next/image";
 
 const boxes = [
   {
-    icon: "/assets/Location.svg",
+    icon: "/assets/LocationWhite.svg",
     title: "Address",
     value: "Dhaka Bangladesh",
   },
   {
-    icon: "/assets/call.svg",
+    icon: "/assets/callWhite.svg",
     title: "Contact",
     value: "+165045707551",
   },
   {
-    icon: "/assets/mail.svg",
+    icon: "/assets/mailWhite.svg",
     title: "Email",
     value: "Contact@Leadhuntio.Com",
   },
@@ -24,17 +24,24 @@ export default function ContactInfoBoxes() {
       {boxes.map((box) => (
         <div
           key={box.title}
-          className="relative bg-[#0B1746] border-2 border-[#1A4D9B] rounded-2xl flex flex-col items-center justify-center px-10 py-8 shadow-lg"
-          style={{ minWidth: 260, boxShadow: "0 0 24px 0 #1A4D9B" }}
+          className="relative bg-[#032356] border-2 border-[#085592] rounded-2xl flex flex-col items-center justify-center shadow-lg"
+          style={{
+            width: 365,
+            height: 230,
+            boxShadow: "0 0 24px 0 #1A4D9B",
+            padding: "2rem 2.5rem",
+          }}
         >
-          <div className="absolute -top-10 left-1/2 -translate-x-1/2 bg-[#1A4D9B] rounded-full p-4 shadow-lg">
+          <div className="absolute -top-10 left-1/2 -translate-x-1/2 bg-[#024EC2] rounded-full p-4 shadow-lg">
             <Image src={box.icon} alt={box.title} width={48} height={48} />
           </div>
           <div className="mt-8 text-center">
-            <div className="text-white text-2xl font-bold mb-2">
+            <div className="text-white text-[28px] font-bold mb-2">
               {box.title}
             </div>
-            <div className="text-[#B8D0F6] text-lg break-all">{box.value}</div>
+            <div className="text-[#D8D8D8] text-[19px] break-all">
+              {box.value}
+            </div>
           </div>
         </div>
       ))}
